75333d02f0e8f158fab4d477b1789d2c94980911
JasmineW2812 <c3021544@my.shu.ac.uk>
2024-11-10 20:03:44 +0000

    small-fix-admin

--- my-app/resources/views/admin-menu.blade.php
+++ my-app/resources/views/admin-menu.blade.php
@@ -9,6 +9,7 @@
     <div class="dropdown-menu show dropdown-menu-green" aria-labelledby="dropdownMenuButton">
         <h6 class="dropdown-header">Home</h6>
         <a class="dropdown-item" href="{{ route('admin.dashboard') }}">Dashboard</a>
+        <div class="dropdown-divider"></div>
         <h6 class="dropdown-header">Account Manager</h6>
         <a class="dropdown-item" href="{{ route('admin.createStaff') }}">Add staff account</a>
         <a class="dropdown-item" href="{{ route('admin.searchStaff') }}">Search staff account</a>
