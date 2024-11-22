e8bc727ca5be7e886ded3d1d1019b8b483417830
JasmineW2812 <c3021544@my.shu.ac.uk>
2024-11-22 18:47:26 +0000

    simplifying functionality of admin inventory

--- my-app/resources/views/admin-inventory-data.blade.php
+++ my-app/resources/views/admin-inventory-data.blade.php
@@ -56,13 +56,7 @@
             <div class="col-md-6 mb-4">
               <div class="card" style="width: 18rem;">
                 <div class="card-body position-relative">
-                  <h5 class="card-text">{{ $product->name }}<i class="fa-solid fa-plus"
-                  type= button
-                  class="btn btn-primary" 
-                  data-bs-toggle="modal" 
-                  data-bs-target="#productModal"
-                  data-name="{{ $product->name}}">
-                  </i></h5>
+                  <h5 class="card-text">{{ $product->name }}</h5>
                   <h5 class="card-text">{{ "number go up or down" }} </h5>
                   <input type="checkbox" class="position-absolute top-0 end-0 me-2 mt-2">
 
@@ -72,32 +66,7 @@
           @endforeach
         </div>
       @endforeach
-      
-      </div>
-    </div>
 
-    <div class="modal fade" id="productModal" tabindex="-1" aria-labelledby="productModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="modalTitle">Product Details</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="mb-3">
-          <label for="modalName" class="form-label"><strong>Name:</strong></label>
-        </div>
-        <div class="mb-3">
-          <label for="modalImage" class="form-label"><strong>Image:</strong></label>
-        </div>
-        <div class="mb-3">
-          <label for="modalStockAvailable" class="form-label"><strong>Stock Available:</strong></label>
-        </div>
-        <div class="mb-3">
-          <label for="modalStockData" class="form-label"><strong>Buying trends for this product:</strong></label>
-        </div>
-      </div>
-    </div>
   </div>
 </div>
 
@@ -111,22 +80,3 @@
 
 @endsection
 
-<script>
-document.addEventListener('DOMContentLoaded', () => {
-  const productModalId = document.getElementById('productModal');
-
-  productModalId.addEventListener('show.bs.modal', function (event) {
-    const button = event.relatedTarget;
-
-    const Name = button.getAttribute('data-name');
-    const Image = button.getAttribute('data-image');
-    const email = button.getAttribute('data-email');
-    const role = button.getAttribute('data-role');
-
-    document.getElementById('modalName').value = Name;
-    document.getElementById('modalImage').value = Image;
-    document.getElementById('modalEmailInput').value = email;
-    document.getElementById('modalRoleInput').value = role;
-  });
-});
-</script>
