c7ddcc4374501e5825e57c2b7fb2dab8f8d3b2ae
c3052701 <c3052701@hallam.shu.ac.uk>
2024-11-26 19:45:30 +0000

    signup

--- my-app/resources/views/sign-up.blade.php
+++ my-app/resources/views/sign-up.blade.php
@@ -9,7 +9,34 @@
     <title>Sign-Up</title>
 </head>
 <body>
-    Helloo
+    <div class="container">
+        <form action="" class="form-signup">
+            <h2>Create an Account</h2>
+
+            <div class= "form-group">
+                <div class="row">
+                    <div class="col-md-6">
+                        <input type="text" name="firstname" placeholder="Enter your first name">
+                    </div>
+                    <div class="col-md-6">
+                        <input type="text" name="lasttname" placeholder="Enter your last name">
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <input type="email" name="email" placeholder="Email address">
+            </div>
+
+            <div class="form-group">
+                <input type="password" name="password" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <input type="repassword" name="confirm_password" placeholder="Confirm Password">
+            </div>
+        </form>
+
+    </div>
 </body>
 </html>
 @endsection
