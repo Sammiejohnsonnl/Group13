6c4a2c278ad3be120ba1f667ce4fcf63a9485cda
JasmineW2812 <c3021544@my.shu.ac.uk>
2024-12-01 23:03:38 +0000

    models adding relationship/fillable

--- my-app/app/Models/Order.php
+++ my-app/app/Models/Order.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
-    
+    protected $fillable = ['registered_customer_id', 'total', 'delivery_status']; 
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(RegisteredCustomer::class, 'registered_customer_id');
+    }
+
+    public function productOrders(): HasMany
+    {
+        return $this->hasMany(ProductOrdered::class, 'order_id');
+    }
 }
+
--- my-app/app/Models/Product.php
+++ my-app/app/Models/Product.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['image_path','product_type','name','platform', 'price', 'stock_quantity'];
+
+    public function productOrders(): HasMany
+    {
+        return $this->hasMany(ProductOrdered::class, 'product_id');
+    }
 }
