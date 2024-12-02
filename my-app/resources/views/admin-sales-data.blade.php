62bdc4e5105cf86ee8c802312ea904f27606873a
JasmineW2812 <c3021544@my.shu.ac.uk>
2024-12-02 15:47:27 +0000

    migrations for customer/admin sales page

--- my-app/resources/views/admin-sales-data.blade.php
+++ my-app/resources/views/admin-sales-data.blade.php
@@ -4,38 +4,63 @@
 
 @include('admin-menu')
 
-    <header>
-        <h1>Inventory</h1>
-        <div class="header-icons">
-            <a href="{{ route('notification') }}"><i class="fa-regular fa-bell"></i></a>
-            <i class="fa-regular fa-user header-icons"></i>
-        </div>
-    </header>
-    <div class="content">
-        <h3>Products</h3>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Product</th>
-                    <th scope="col">Type</th>
-                    <th scope="col">Platform</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Stock</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($ as $index => $)
-                    <tr>
-                        <th scope="row">{{ $index + 1 }}</th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td> 
-                        <td></td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+<header>
+    <h1>Inventory</h1>
+    <div class="header-icons">
+        <a href="{{ route('notification') }}"><i class="fa-regular fa-bell"></i></a>
+        <i class="fa-regular fa-user header-icons"></i>
     </div>
+</header>
+
+<div class="content">
+    <h3>Invoices</h3>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Customer Name</th>
+                <th scope="col">Products</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Total</th>
+                <th scope="col">Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($orders as $order)
+                <tr>
+                    <th scope="row">{{ $order->id }}</th>
+                    <td>
+                        <a href="{{ route('customers.show', $order->customer->id) }}">
+                            {{ $order->customer->first_name }}
+                        </a>
+                    </td>
+                    <td>
+                        @foreach($order->products as $product)
+                            <div>
+                                <a href="{{ route('products.show', $product->id) }}">
+                                    {{ $product->name }}
+                                </a>
+                            </div>
+                        @endforeach
+                    </td>
+                    <td>
+                        @foreach($order->products as $product)
+                            <div>{{ $product->pivot->ordered_quantity }}</div>
+                        @endforeach
+                    </td>
+
+                    <td>{{ number_format($order->total, 2) }}</td>
+                    <td>
+                    @if($order->created_at)
+                        {{ $order->created_at->format('Y-m-d') }}
+                    @else
+                        <span>Unknown date</span>
+                    @endif
+                </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
 @endsection
